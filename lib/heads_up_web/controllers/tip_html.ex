9a6db73fa5acb56e9c0f74edcb452b57de8474f7
Santiago Gonzalez <santiago.gonzalez@mimiquate.com>
2025-12-17 15:51:53 -0300

    feat(main): Add conn log and a new /tips controller

---
+++ lib/heads_up_web/controllers/tip_html.ex
@@ -0,0 +1,6 @@
+defmodule HeadsUpWeb.TipHTML do
+  use HeadsUpWeb, :html
+
+  embed_templates "tip_html/*"
+end
+
--- lib/heads_up_web/router.ex
+++ lib/heads_up_web/router.ex
@@ -8,22 +8,33 @@ defmodule HeadsUpWeb.Router do
     plug :put_root_layout, html: {HeadsUpWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :snoop
   end
 
   pipeline :api do
     plug :accepts, ["json"]
   end
 
+  def snoop(conn, _opts) do
+    answer = ~w(Yes No Maybe) |> Enum.random()
+
+    conn = assign(conn, :greeting, answer)
+
+    IO.inspect(conn)
+    conn
+  end
+
   scope "/", HeadsUpWeb do
     pipe_through :browser
 
     get "/", PageController, :home
+    get "/tips", TipController, :index
   end
 
   # Other scopes may use custom stacks.
-  # scope "/api", HeadsUpWeb do
-  #   pipe_through :api
-  # end
+  scope "/api", HeadsUpWeb do
+    pipe_through :api
+  end
 
   # Enable LiveDashboard and Swoosh mailbox preview in development
   if Application.compile_env(:heads_up, :dev_routes) do
