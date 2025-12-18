e1971f4d4e74cda74554c7dd33974f6bacc472c1
Santiago Gonzalez <santiago.gonzalez@mimiquate.com>
2025-12-18 14:11:49 -0300

    feat(main): Add three responders every 2 seconds using Process.send_after.

--- lib/heads_up_web/live/effort_live.ex
+++ lib/heads_up_web/live/effort_live.ex
@@ -2,6 +2,10 @@ defmodule HeadsUpWeb.EffortLive do
   use HeadsUpWeb, :live_view
 
   def mount(_params, _session, socket) do
+    if connected?(socket) do
+      Process.send_after(self(), :tick, 2000)
+    end
+
     socket = assign(socket, responders: 0, minutes_per_responder: 10)
 
     {:ok, socket}
@@ -46,5 +50,10 @@ defmodule HeadsUpWeb.EffortLive do
     socket = assign(socket, :minutes_per_responder, String.to_integer(minutes))
 
     {:noreply, socket}
+  end
+
+  def handle_info(:tick, socket) do
+    Process.send_after(self(), :tick, 2000)
+    {:noreply, update(socket, :responders, &(&1 + 3))}
   end
 end
