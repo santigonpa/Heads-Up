00f86d87f51b6d033553b764f02f8ebbb596941d
Santiago Gonzalez <santiago.gonzalez@mimiquate.com>
2025-12-19 12:09:32 -0300

    feat(main): Add function components and attr definitions.

--- lib/heads_up_web/live/incident_live/index.ex
+++ lib/heads_up_web/live/incident_live/index.ex
@@ -11,22 +11,36 @@ defmodule HeadsUpWeb.IncidentLive.Index do
 
   def render(assigns) do
     ~H"""
-      <div class="incident-index">
-        <div class="incidents">
-         <div :for={incident <- @incidents} class="card">
-           <img src={incident.image_path}/>
-            <h2><%= incident.name %></h2>
-            <div class="details">
-              <div class="name">
-                <%= incident.priority %>
-              </div>
-              <div class="badge">
-                <%= incident.status %>
-              </div>
-            </div>
-          </div>
-        </div>
+    <div class="incident-index">
+      <div class="incidents">
+        <.incident_card :for={incident <- @incidents} incident={incident}/>
       </div>
+    </div>
+    """
+  end
+
+  attr :incident, HeadsUp.Incidents
+  def incident_card(assigns) do
+    ~H"""
+    <div class="card">
+      <img src={@incident.image_path}/>
+      <h2><%= @incident.name %></h2>
+      <div class="details">
+        <div class="name">
+          <%= @incident.priority %>
+        </div>
+        <.badge status={@incident.status} />
+      </div>
+    </div>
+    """
+  end
+
+  attr :status, :atom, required: true, values: [:pending, :resolved, :cancelled]
+  def badge(assigns) do
+    ~H"""
+    <div class="rounded-md px-2 py-1 text-xs font-medium uppercase inline-block border text-lime-600 border-lime-600">
+      <%= @status %>
+    </div>
     """
   end
 end
